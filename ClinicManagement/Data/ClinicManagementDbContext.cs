5bacacda4f6c1672a90352cf366e7a2c8f2f05c8
abel masingita <60102940+abelmasingita@users.noreply.github.com>
2023-02-16 17:03:13 +0200

    Add project files.

---
+++ ClinicManagement/Data/ClinicManagementDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ClinicManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicManagement.Data
+{
+    public class ClinicManagementDbContext : DbContext
+    {
+        public ClinicManagementDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Doctor> Doctors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ClinicManagement/Models/Doctor.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClinicManagement.Models
+{
+    public class Doctor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid doctor_id { get; set; }
+        public string name { get; set; }
+        public string speciality { get; set; }
+        public string contact { get; set; }
+        public string email { get; set; }
+    }
+}
